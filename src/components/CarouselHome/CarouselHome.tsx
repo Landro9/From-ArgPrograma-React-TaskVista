56dc76899ecdfafdc3734a23d3159729e8cad827
Landro9 <leandro_15_carc@hotmail.com>
2023-11-27 11:04:03 -0300

    Arreglo

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -12,21 +12,21 @@ const CarouselHome = ()=> {
     return(
          <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img src="src/assets/Image/slide1.jpg" alt="" />
+        <img src="/assets/Image/slide1.jpg" alt="" />
         <Carousel.Caption>
           <h3>Somos el mejor equipo de Aplicaciones Agilesl</h3>
           <p>Te Brindamos todo el ascesorameinto en Desarrollo de Software</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="src/assets/Image/slide2.jpg" alt="" />
+        <img src="/assets/Image/slide2.jpg" alt="" />
         <Carousel.Caption>
           <h3>Los mejores expertos del país</h3>
           <p>Trabajamos con las útimas tecnologías del Front y back cada día.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="src/assets/Image/slide3.jpg" alt="" />
+        <img src="/assets/Image/slide3.jpg" alt="" />
         <Carousel.Caption>
           <h3>Potencia tu negocio con nuestras soluciones personalizadas</h3>
           <p>Desarrollamos software a medida para satisfacer tus necesidades empresariales específicas.</p>
